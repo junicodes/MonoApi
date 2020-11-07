8befd881a4185e094db11e54f4022fd8ff38cafe
junicodefire <junicodefire@gmail.com>
2020-11-07 07:56:24 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import MonoConnect from '@mono.co/connect.js';
+import './App.css';
 
 export default function App() {
   const monoConnect = React.useMemo(() => {
     const monoInstance = new MonoConnect("test_pk_C2ozPjWOCkytgJKwM2M9", {
       onClose: () => console.log('Widget closed'),
       onLoad: () => console.log('Widget loaded successfully'),
-      onSuccess: ({ code }) => console.log(`Linked successfully: ${code}`)
+      onSuccess: ({ code }) => {
+        console.log(`Linked successfully: ${code}`)
+        window.parent.postMessage({ message: "getAppData", value: `Linked successfully: ${code}`, authCode: code }, "*");
+       
+      }
     })
 
     monoInstance.setup()
@@ -14,11 +19,18 @@ export default function App() {
     return monoInstance;
   }, [])
 
+  if(window.origin){
+    monoConnect.open()
+  }
+
   return (
     <div>
-      <button onClick={() => monoConnect.open()}>
+      {/* <button onClick={() => monoConnect.open()}>
         Authenticate with Mono
-      </button>
+      </button> */}
+      {/* <button onClick={() => monoConnect.open()}>
+        Authenticate with Mono
+      </button> */}
     </div>
   )
 }
